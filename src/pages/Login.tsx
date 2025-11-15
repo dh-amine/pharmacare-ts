820c3e6f4c729c98238751a17fdec2f5db165099
dh-amine <127792121+dh-amine@users.noreply.github.com>
2025-11-15 19:15:53 +0100

    Updated login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,6 +5,7 @@ const LoginPage = () => {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
     <div className="w-full max-w-sm">
+      <div>Login page</div>
       <LoginForm />
     </div>
   </div>
--- src/pages/MedsPage.tsx
+++ src/pages/MedsPage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MedsPage = () => {
   return (
     <div>
-        meds page
+        meds pages
     </div>
   )
 }
